2295f6b007e8c7f655edd759d818f16af3a73f0c
Oscar Cabellos <oscar.cabellos@unmsm.edu.pe>
2020-12-21 11:48:41 -0500

    master desincrosiado

--- src/services/addUser.js
+++ src/services/addUser.js
@@ -1,13 +1,18 @@
 function aadUser({ formData = {}, loader = false } = {}) {
   // const API = 'https://nodecrudsandersonapi.herokuapp.com/api/users/'
-  const API = 'https://nodecrudsandersonapi.herokuapp.com/api/users/'
-  // const API = 'http://localhost:4000/api/users/'
+  // const API = 'https://nodecrudsandersonapi.herokuapp.com/api/users/'
+  const API = 'https://api-login-heroku.herokuapp.com/signup'
+  // const API = 'http://localhost:4000/signup'
   return fetch(API, {
     method: 'POST',
     body: formData
   })
     .then(res => res.json())
-    .then(response => response.message)
+    .then(response => {
+      console.log(response)
+      localStorage.setItem("usuarioActual", JSON.stringify(response.data));
+      return response.message
+    })
 }
 
 export default aadUser
--- src/services/getUsers.js
+++ src/services/getUsers.js
@@ -1,7 +1,7 @@
 function sendUser({ formData = {}, loader = false } = {}) {
-  const API = 'https://nodecrudsandersonapi.herokuapp.com/api/users/login'
-  // const API = 'https://api-login-heroku.herokuapp.com/signin'
-  // const API = 'http://localhost:4000/api/users/login'
+  // const API = 'https://nodecrudsandersonapi.herokuapp.com/api/users/login'
+  const API = 'https://api-login-heroku.herokuapp.com/signin'
+  // const API = 'http://localhost:4000/signin'
   return fetch(API, {
     method: 'POST',
     body: formData
@@ -9,9 +9,9 @@ function sendUser({ formData = {}, loader = false } = {}) {
     .then(res => res.json())
     .then(response =>{
       console.log(response);
-      localStorage.setItem("usuarioActual", JSON.stringify(response.data.validate[0]));
+      localStorage.setItem("usuarioActual", JSON.stringify(response.data));
       // console.log(response.data.validate);
-      const validate = response.data.valid
+      const validate = response.valid
       loader = false
       return {validate, loader}
     })
