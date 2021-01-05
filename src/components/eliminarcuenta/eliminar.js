46ffa1c24f6396bf7064f66b6a17ec3145cb3487
Oscar Cabellos <oscar.cabellos@unmsm.edu.pe>
2021-01-05 13:35:37 -0500

    arreglo en cuenta

--- src/components/eliminarcuenta/eliminar.js
+++ src/components/eliminarcuenta/eliminar.js
@@ -3,62 +3,63 @@ import React from 'react'
 import Swal from 'sweetalert2'
 import './eliminar.css'
 
-const Eliminar = () => {
-	const swalWithBootstrapButtons = Swal.mixin({ 
-		customClass: {
-			confirmButton: 'btn btn-success',
-			cancelButton: 'btn btn-danger'
-		},
-		buttonsStyling: false
-	})
-	swalWithBootstrapButtons.fire ({
-		title: 'Opciones de Cuenta',
-  		text: "Usted va a eliminar su cuenta",
-  		icon: 'warning',
-  		showCancelButton: true,
-  		confirmButtonText: 'Eliminar',
-		cancelButtonText: 'Cancelar',
-		reverseButtons: true,
-		footer: '<span class="rojo">Su cuenta se eliminara permanente </span>',
-		backdrop: false,
-		// timer: 8000,
-		// timerProgressBar: true,  
-		buttonsStyling: true,
-		showCloseButton: true, 
-		closeButtonAriaLabel: 'Cerrar alerta'
+function Eliminarcuenta() {
 
-	}).then((result) => {
-		if (result.isConfirmed){
-			swalWithBootstrapButtons.fire(
-				'Eliminado',
-				'Se elimino exitosamente',
-				'success'
-			)
-		} else if (
-			result.dismiss === Swal.DismissReason.cancel
-		) {
-			swalWithBootstrapButtons.fire(
-				'Cancelado',
-				'Cuenta no eliminada',
-				'error'
-			)
-		}
-	})
+	const eliminar = () => {
+		const swalWithBootstrapButtons = Swal.mixin({
+			customClass: {
+				confirmButton: 'btn btn-success',
+				cancelButton: 'btn btn-danger'
+			},
+			buttonsStyling: false
+		})
+		swalWithBootstrapButtons.fire({
+			title: 'Opciones de Cuenta',
+			text: "Usted va a eliminar su cuenta",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonText: 'Eliminar',
+			cancelButtonText: 'Cancelar',
+			footer: '<span className="rojo">Su cuenta se eliminara permanente </span>',
+			reverseButtons: true,
+			backdrop: true,
+			timer: 5000,
+			timerProgressBar: true,
+			buttonsStyling: true,
+			showCloseButton: true,
+			closeButtonAriaLabel: 'Cerrar alerta'
+		}).then((result) => {
+			if (result.isConfirmed) {
+				swalWithBootstrapButtons.fire(
+					'Eliminado',
+					'Se elimino exitoxamente',
+					'success'
+				)
+			} else if (
+				result.dismiss === Swal.DismissReason.cancel
+			) {
+				swalWithBootstrapButtons.fire(
+					'Cancelado',
+					'Se cancelo el proceso',
+					'error'
+				)
+			}
+		})
+	}
 
-    return (
-        <>
+	return (
+		<>
 			<h1>Opciones de Cuenta</h1>
 			<h2>Eliminacion de cuenta</h2>
 			<div>
 				<h3>Confirmar contraseña</h3>
-				<input type="password"></input>	
+				<input type="password"></input>
 			</div>
-		
-			<input type= "button" onClick={Eliminar} value= "Eliminar" > </input> 
-        </>
 
-    )
-    
+			<button onClick={eliminar}>Eliminar</button>
+		</>
+
+	)
 }
 
-export default Eliminar
+export default Eliminarcuenta
--- src/components/preferences/preferences.js
+++ src/components/preferences/preferences.js
@@ -20,12 +20,12 @@ function Preferences({preferences}){
       case '3':
         return <div className="preference 3">3</div>
       case '4':
-        return <div className="preference 4">
-          <Eliminar/>
-        </div>    
+        return <div className="preference 4">4
+        </div>
       case '5':
         return <div className="preference 5">
-          <Cuenta/>
+          <Eliminar/>
+          {/* <Cuenta/> */}
         </div>
       default:
         return null;
@@ -42,7 +42,7 @@ function Preferences({preferences}){
               <p id="1" onClick={showPreferences}>Editor</p>
               <p id="2" onClick={showPreferences}>Descargas</p>
               <p id="3" onClick={showPreferences}>Opcion 3</p>
-              <p id="4" onClick={showPreferences}>Eliminar Cuenta</p>
+              <p id="4" onClick={showPreferences}>Cuenta</p>
               <p id="5" onClick={showPreferences}>Opcion 5</p>
             </div>
             <div className="preferences">
