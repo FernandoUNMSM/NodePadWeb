2d7c557188446c37d9f18958b1b85fb06e6c8584
Sergio-Gual <sergiogual.istf@gmail.com>
2021-01-04 23:42:43 -0500

    falta

---
+++ src/components/eliminarcuenta/eliminarcuenta.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import Swal from 'sweetalert2'
+import './eliminarcuenta.css'
+function Eliminarcuenta() {
+    Swal.fire({
+        title: "Opciones de Cuenta",
+        text: "Usted va a eliminar su cuenta",
+        icon: 'warning'
+	// confirmButtonText:
+	// footer:
+	// width:
+	// padding:
+	// background:
+	// grow:
+	// backdrop:
+	// timer:
+	// timerProgressBar:
+	// toast:
+	// position:
+	// allowOutsideClick:
+	// allowEscapeKey:
+	// allowEnterKey:
+	// stopKeydownPropagation:
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	// imageUrl:
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+
+    });
+
+    
+    return (
+        <>
+
+
+
+
+
+        </>
+
+    )
+    
+}
+  export default Eliminarcuenta
